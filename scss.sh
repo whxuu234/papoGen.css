e1b7127e2ccc17b91a7ece209b1e39bfa0635470
kevinbird61 <kevinbird61@gmail.com>
2018-05-04 16:21:04 +0800

    Change the test/ to docs/
    * In order to adapt the github.io

--- scss.sh
+++ scss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Program: Compile papogen.scss and generate papogen.css file in /lib/asset/ 
 
-sass scss/papogen.scss test/css/papogen.css
+sass scss/papogen.scss docs/css/papogen.css
 echo SCSS compiled successful. 
