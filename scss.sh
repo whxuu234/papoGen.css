81d6b50cb4a8d9eb2d8747bbe7c28620e0a25986
yungshenglu <yungshenglu1994@gmail.com>
2018-05-06 19:03:17 +0800

    Define global color variables

--- scss.sh
+++ scss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Program: Compile papogen.scss and generate papogen.css file in /lib/asset/ 
 
-sass scss/papogen.scss docs/css/papogen.css
+sass scss/papogen.scss docs/css/papogen.css --quiet
 echo Sass/SCSS compiled finished. 
