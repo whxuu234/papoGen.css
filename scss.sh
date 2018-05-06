4ab999dd3d91352eafd6111329dbe4641717a7d7
yungshenglu <yungshenglu1994@gmail.com>
2018-05-06 16:11:19 +0800

    Fix out some site definitions

--- scss.sh
+++ scss.sh
@@ -2,4 +2,4 @@
 # Program: Compile papogen.scss and generate papogen.css file in /lib/asset/ 
 
 sass scss/papogen.scss docs/css/papogen.css
-echo SCSS compiled successful. 
+echo Sass/SCSS compiled finished. 
