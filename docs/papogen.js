42e9e95889de134b01f75167ef0d58e35ed30b0d
abvesa <elunivch@gmail.com>
2018-05-15 16:02:44 +0800

    test(js): make inherit as function

--- docs/papogen.js
+++ docs/papogen.js
@@ -10,23 +10,24 @@
 
 // button
 
+function inherit () {
+  for (let i = 0; i < $('.buttons').length; i++) {
+    let curr = $('.buttons')[i]
+    let curr_class = curr.classList.value
+    let child_class = curr_class.replace('horizontal', '')
+                                .replace('vertical', '')
+                                .replace('buttons', '')
+                                .trim().replace(/  +/g, ' ')
+    // add child class
+    $(curr.children).addClass(child_class)
 
-
-
-for (let i = 0; i < $('.buttons').length; i++) {
-  let curr = $('.buttons')[i]
-  let curr_class = curr.classList.value
-  let child_class = curr_class.replace('horizontal', '')
-                              .replace('vertical', '')
-                              .replace('buttons', '')
-                              .trim().replace(/  +/g, ' ')
-  // add child class
-  $(curr.children).addClass(child_class)
-
-  // remove child class from parent
-  for (let name of child_class.split(' ')) {
-    curr.removeClass(name)
+    // remove child class from parent
+    for (let name of child_class.split(' ')) {
+      $(curr).removeClass(name)
+    }
   }
 }
 
-
+$(document).ready(() => {
+  inherit()
+})
